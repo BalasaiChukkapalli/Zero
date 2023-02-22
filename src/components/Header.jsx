79f1c4342ddd36ff13f970adb8f5eb0b989ec157
BALA SAI CHUKKAPALLI <90022963+BalasaiChukkapalli@users.noreply.github.com>
2023-02-22 19:14:18 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -16,7 +16,7 @@ const Header = () => {
         className="flex justify-start items-center
       text-xl text-black space-x-1"
       >
-        <span>Genesis</span>
+        <span>Fund Raiser</span>
         <TbBusinessplan />
       </Link>
 
